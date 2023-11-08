47e32f36e9953e5bf4a24d898d9bba522ff9f269
ChenghaoLi <lichenghaongyd@qq.com>
2023-11-08 07:46:28 +0000

    build(namer): 添加常量传播中对逻辑返回值和运算数返回值的区别

--- frontend/namer/src/complie_calculate.rs
+++ frontend/namer/src/complie_calculate.rs
@@ -28,6 +28,19 @@ fn error_binary_op(
 	)
 }
 
+
+fn error_unary_op(
+
+	op: &str,
+	rhs: &CompileConstValue,
+) -> String {
+	format!(
+		"invalid operands of type and {} to  unary{}",
+		get_type(rhs),
+		op
+	)
+}
+
 fn value_unwarp_f32(x: &CompileConstValue) -> Option<f32> {
 	match x {
 		CompileConstValue::Int(v) => Some(*v as f32),
@@ -42,6 +55,7 @@ fn x_op_y(
 	op_name: &str,
 	int_op: fn(i32, i32) -> i32,
 	float_op: fn(f32, f32) -> f32,
+	logical : bool
 ) -> Result<CompileConstValue, SysycError> {
 	if let (CompileConstValue::Int(x), CompileConstValue::Int(y)) = (lhs, rhs) {
 		return Ok(CompileConstValue::Int(int_op(*x, *y)));
@@ -51,7 +65,40 @@ fn x_op_y(
 		value_unwarp_f32(&lhs).ok_or(SysycError::SyntaxError(err_msg.clone()))?;
 	let y =
 		value_unwarp_f32(&rhs).ok_or(SysycError::SyntaxError(err_msg.clone()))?;
-	Ok(CompileConstValue::Float(float_op(x, y)))
+	if logical {
+		if float_op(x,y) == 0.0 {
+			Ok(CompileConstValue::Int(0))
+		} else {
+			Ok(CompileConstValue::Int(1))
+		}
+
+	} else {
+		Ok(CompileConstValue::Float(float_op(x, y)))
+	}
+}
+
+fn op_y(
+	rhs : &CompileConstValue,
+	op_name : &str,
+	int_op: fn(i32) -> i32,
+	float_op : fn(f32) -> f32,
+	logical : bool
+) -> Result<CompileConstValue, SysycError> {
+	if let CompileConstValue::Int(x) = rhs {
+		Ok(CompileConstValue::Int(int_op(*x)))
+	} else if let CompileConstValue::Float(x) = rhs {
+		if logical{
+			if float_op(*x) == 0.0{
+				Ok(CompileConstValue::Int(0))
+			} else {
+				Ok(CompileConstValue::Int(1))
+			}
+		} else {
+			Ok(CompileConstValue::Float(float_op(*x)))
+		}
+	} else {
+		Err(SysycError::SyntaxError(error_unary_op(op_name, rhs)))
+	}
 }
 
 pub fn evaluate_binary(
@@ -62,9 +109,39 @@ pub fn evaluate_binary(
 	// println!("{:?}, {:?}, {:?}", lhs, op ,rhs);
 	match op {
 		BinaryOp::Add => {
-			x_op_y(lhs, rhs, "add", |x, y| x.wrapping_add(y), |x, y| x + y)
-		}
+			x_op_y(lhs, rhs, "add", |x, y| x.wrapping_add(y), |x, y| x + y, false)
+		},
+		BinaryOp::Assign => {
+			x_op_y(lhs, rhs, "assign", |_x, y| y, |_x, y| y, false)
+		},
+		BinaryOp::Div => {
+			x_op_y(lhs, rhs, "div", |x, y| x.wrapping_div(y), |x, y| x/y, false)
+		},
+		BinaryOp::EQ => {
+			x_op_y(lhs, rhs, "eq", |x, y| if x == y {1} else {0}, |x, y|if x == y {1.0} else {0.0}, true)
+		},
+		BinaryOp::GE => {
+			x_op_y(lhs, rhs, "ge", |x, y| if x >= y {1} else {0}, |x, y|if x >= y {1.0} else {0.0}, true)
+		},
+		// BinaryOp::GQ
 
 		_ => todo!(),
+	}
+}
+
+pub fn evaluate_uniary(
+	op : &UnaryOp,
+	rhs : &CompileConstValue
+) -> Result<CompileConstValue, SysycError> {
+	match op{
+		UnaryOp::Neg => {
+			op_y(rhs, "neg", |x| x.wrapping_neg(), |x| -x, false)
+		},
+		UnaryOp::Plus => {
+			op_y(rhs, "plus", |x| x, |x| x, false)
+		},
+		UnaryOp::Not => {
+			op_y(rhs, "not", |x| match x {0 => 1, _ => 0}, |x| {if x == 0.0 {0.0} else {1.0}}, true)
+		}
 	}
 }
---
+++ test.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+
+
+
+int main(){
+
+    int a = 2; int b = 3;
+    if(a > b){}
+    if(a >= b){}
+    if(a < b){}
+    if(a <= b){}
+}
