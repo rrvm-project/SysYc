1e34bb6d90806d036779d6c05fec840cc08b5aa4
Fuyuki <3216214161@qq.com>
2023-11-08 19:38:50 +0800

    feat(utils): grammar file of IR

--- utils/src/errors.rs
+++ utils/src/errors.rs
@@ -6,6 +6,8 @@ pub enum SysycError {
 	DecafLexError(String),
 	#[error("system error: {0}")]
 	SystemError(String),
+	#[error("llvm syntex error: {0}")]
+	LlvmSyntexError(String),
 }
 
 pub fn map_sys_err(e: std::io::Error) -> SysycError {
