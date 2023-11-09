ef3a696d739ffb41a85f5528db264c56194ac1a6
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-11-10 00:03:05 +0800

    chore: test cargo fmt in github action

--- frontend/ir_gen/src/llvmirgen.rs
+++ frontend/ir_gen/src/llvmirgen.rs
@@ -1,6 +1,6 @@
 use llvm::{LlvmProgram, llvmfuncemitter::LlvmFuncEmitter};
 use utils::SysycError;
-use ast::{tree::Program};
+use ast::{tree::Program, visitor::Visitor};
 pub struct LlvmIrGen {
     pub funcemitter: LlvmFuncEmitter,
 }
@@ -10,5 +10,15 @@ impl LlvmIrGen {
         program.comp_units.iter().for_each(|comp_unit| {
             comp_unit.accept(self);
         });
+        Ok(LlvmProgram{
+            funcs: vec![],
+            // funcs: vec![self.funcemitter.emit_func()],
+            global_vars: vec![],
+        
+        })
     }
+}
+
+impl Visitor for LlvmIrGen {
+    
 }
