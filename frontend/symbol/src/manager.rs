6c29457c6570a89bce6a483c9ea25baec843ed69
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-11-21 22:12:14 +0800

    refa(irgen): 实现数组的初始化列表和全局变量的 IR 生成

--- frontend/symbol/src/manager.rs
+++ frontend/symbol/src/manager.rs
@@ -15,6 +15,7 @@ impl SymbolManager {
 		Self { cnt: 0 }
 	}
 	/// `ident` is `Some` only when the symbol is global
+	/// cyh: this rule seems to be violated in namer
 	pub fn new_symbol<T: Clone>(
 		&mut self,
 		ident: Option<String>,
@@ -24,6 +25,7 @@ impl SymbolManager {
 		Symbol {
 			id: self.cnt,
 			var_type,
+			is_global: false,
 			ident: ident.unwrap_or(self.cnt.to_string()),
 		}
 	}
