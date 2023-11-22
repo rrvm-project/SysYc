79002c15077e34692d1e6cf4d1a4c62bec2109b1
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-11-22 13:21:33 +0800

    #22 fix(irgen): 改用HashMap存CFG中的BasicBlock，方便LVN实现

--- utils/llvm/src/cfg.rs
+++ utils/llvm/src/cfg.rs
@@ -1,10 +1,11 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use crate::basicblock::BasicBlock;
 
 #[allow(unused)]
 pub struct CFG {
-	pub basic_blocks: Vec<BasicBlock>,
+	// id 到 basicblock 的映射
+	pub basic_blocks: HashMap<usize, BasicBlock>,
 	pub entry: usize,
 	pub exit: usize,
 }
@@ -13,26 +14,26 @@ impl CFG {
 	pub fn new(entry: BasicBlock, exit: BasicBlock) -> CFG {
 		CFG {
 			entry: entry.id,
-			basic_blocks: vec![entry, exit],
+			basic_blocks: {
+				let mut map = HashMap::new();
+				map.insert(entry.id, entry);
+				map.insert(exit.id, exit);
+				map
+			},
 			exit: 0,
 		}
-	}
-
-	pub fn set_exit(&mut self, exit: BasicBlock) {
-		self.exit = exit.id;
-		self.basic_blocks.push(exit);
 	}
 }
 
 impl Display for CFG {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		for bb in &self.basic_blocks {
-			if bb.id == 1 {
+		for (id, bb) in &self.basic_blocks {
+			if *id == 1 {
 				continue;
 			}
 			writeln!(f, "{}", bb)?;
 		}
-		write!(f, "{}", self.basic_blocks[1])?;
+		write!(f, "{}", self.basic_blocks.get(&1).unwrap())?;
 		Ok(())
 	}
 }
