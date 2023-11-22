e7af80ecda865804187d7148afa9080f5320afdc
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-11-22 18:59:36 +0800

    fix(irgen): #22

--- frontend/irgen/src/utils.rs
+++ frontend/irgen/src/utils.rs
@@ -1,24 +1,54 @@
-use crate::{IRVALUE, VALUE};
+use crate::{irgen::LlvmIrGen, IRVALUE, SYMBOL, VALUE};
 use ast::tree::Node;
 use attr::Attr;
 use llvm::llvmop::Value;
+use rrvm_symbol::VarSymbol;
 use utils::{Result, SysycError};
 
 // 如果有编译器常量则优先返回，否则返回 IRValue
-pub fn get_value(node: &Node) -> Result<Value> {
-	if let Some(Attr::Value(const_value)) = node.get_attr(VALUE) {
-		match const_value {
-			value::Value::Int(v) => Ok(llvm::llvmop::Value::Int(*v)),
-			value::Value::Float(v) => Ok(llvm::llvmop::Value::Float(*v)),
-			_ => Err(SysycError::LlvmSyntexError(format!(
-				"Compile const value should not be {:?}",
-				const_value
-			))),
+
+impl LlvmIrGen {
+	// 如果挂上的 IRValue 是地址，增加一条 load 指令取出地址
+	pub fn get_value(&mut self, node: &Node) -> Result<Value> {
+		if let Some(Attr::Value(const_value)) = node.get_attr(VALUE) {
+			match const_value {
+				value::Value::Int(v) => Ok(Value::Int(*v)),
+				value::Value::Float(v) => Ok(Value::Float(*v)),
+				_ => Err(SysycError::LlvmSyntaxError(format!(
+					"Compile const value should not be {:?}",
+					const_value
+				))),
+			}
+		} else if let Some(Attr::IRValue(ir_value)) = node.get_attr(IRVALUE) {
+			if ir_value.is_ptr() {
+				Ok(Value::Temp(
+					self.funcemitter.as_mut().unwrap().visit_load_instr(ir_value.clone()),
+				))
+			} else {
+				Ok(ir_value.clone())
+			}
+		} else {
+			Err(SysycError::LlvmNoValueError(
+				"node has no value".to_string(),
+			))
 		}
-	} else if let Some(Attr::IRValue(ir_value)) = node.get_attr(IRVALUE) {
-		Ok(ir_value.clone())
-	} else {
-		Err(SysycError::LlvmSyntexError("node has no value".to_string()))
+	}
+	// 用于左操作数，允许直接取出地址
+	pub fn get_lhs_value(&mut self, node: &Node) -> Result<Value> {
+		if let Some(Attr::IRValue(ir_value)) = node.get_attr(IRVALUE) {
+			Ok(ir_value.clone())
+		} else {
+			Err(SysycError::LlvmNoValueError("lhs has no value".to_string()))
+		}
+	}
+	pub fn get_symbol(&mut self, node: &Node) -> Result<VarSymbol> {
+		if let Some(Attr::VarSymbol(symbol)) = node.get_attr(SYMBOL) {
+			Ok(symbol.clone())
+		} else {
+			Err(SysycError::LlvmNoSymbolError(
+				"node has no symbol".to_string(),
+			))
+		}
 	}
 }
 
