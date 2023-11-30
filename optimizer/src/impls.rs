684961b1f1b327baf1b13480673694db4707f4d9
Fuyuki <3216214161@qq.com>
2023-11-30 19:20:09 +0800

    feat(optimizer): remove dead code

--- optimizer/src/impls.rs
+++ optimizer/src/impls.rs
@@ -1,13 +1,34 @@
 use rrvm::program::LlvmProgram;
 use utils::errors::Result;
 
-use crate::{dead_code::RemoveDeadCode, BasicOptimizer, RrvmOptimizer};
+use crate::{RrvmOptimizer, *};
 
-impl RrvmOptimizer for BasicOptimizer {
+impl RrvmOptimizer for Optimizer0 {
 	fn new() -> Self {
 		Self {}
 	}
 	fn apply(self, program: &mut LlvmProgram) -> Result<()> {
-		RemoveDeadCode::new().apply(program)
+		RemoveUnreachCode::new().apply(program)?;
+		Ok(())
+	}
+}
+
+impl RrvmOptimizer for Optimizer1 {
+	fn new() -> Self {
+		Self {}
+	}
+	fn apply(self, program: &mut LlvmProgram) -> Result<()> {
+		RemoveDeadCode::new().apply(program)?;
+		RemoveUnreachCode::new().apply(program)?;
+		Ok(())
+	}
+}
+
+impl RrvmOptimizer for Optimizer2 {
+	fn new() -> Self {
+		Self {}
+	}
+	fn apply(self, _program: &mut LlvmProgram) -> Result<()> {
+		todo!()
 	}
 }
