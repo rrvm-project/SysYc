dae53b02693053b1efa3215f4874a8961c339dd8
ChenghaoLi <lichenghaongyd@qq.com>
2023-12-28 10:26:08 +0000

    feat(middle): 局部表达式重排, #37

--- optimizer/src/impls.rs
+++ optimizer/src/impls.rs
@@ -5,6 +5,7 @@ use crate::{RrvmOptimizer, *};
 use dead_code::RemoveDeadCode;
 use unreachable::RemoveUnreachCode;
 use useless_code::RemoveUselessCode;
+use local_expression_rearrangement::LocalExpressionRearrangement;
 
 impl Optimizer0 {
 	pub fn new() -> Self {
