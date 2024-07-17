24b8f0a7886745c5bb029a21d75ff72e92e88596
Rosayxy <xy-yu22@mails.tsinghua.edu.cn>
2024-07-18 01:30:18 +0800

    fix: fix on instrdag self loop

--- backend/transform/src/instrdag.rs
+++ backend/transform/src/instrdag.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use instruction::riscv::RiscvInstr;
+use instruction::riscv::{value::RiscvTemp, RiscvInstr};
 use rrvm::RiscvNode;
 use std::fmt;
 use utils::SysycError;
@@ -33,28 +33,33 @@ pub struct InstrDag {
 impl InstrDag {
 	pub fn new(node: &RiscvNode) -> Result<Self, SysycError> {
 		let mut nodes: Vec<Node> = Vec::new();
-		let mut defs = HashMap::new();
+		let mut defs:HashMap<RiscvTemp, Rc<RefCell<InstrNode>>> = HashMap::new();
 		let mut uses = HashMap::new();
 		let mut last_call: Option<Node> = None;
 		let mut last_loads: Vec<Node> = Vec::new();
 		let mut last_uses = HashMap::new();
 		for (idx, instr) in node.borrow().instrs.iter().rev().enumerate() {
+			// println!("instr id:{} {}",instr, idx);
 			let node = Rc::new(RefCell::new(InstrNode::new(instr, idx)));
 			let mut instr_node_succ = Vec::new();
 			let instructions_write = instr.get_riscv_write().clone();
-			for instr_write in instructions_write.into_iter() {
+			for instr_write in instructions_write {
 				instr_node_succ.extend(
 					uses.get(&instr_write).unwrap_or(&Vec::new()).iter().cloned(),
 				);
+				// println!("in instr {} write extending..",node.borrow().id);
+				// for i in uses.get(&instr_write).unwrap_or(&Vec::<Rc<RefCell<InstrNode>>>::new()).iter().map(|z| z.borrow().id).collect::<Vec<_>>() {
+				// 	println!("id: {}", i);
+				// }
 				uses.remove(&instr_write);
-				defs.insert(instr_write, node.clone());
 			}
 			let instr_read = instr.get_riscv_read().clone();
 			for instr_read_temp in instr_read.iter() {
 				if let Some(def_instr) = defs.get(instr_read_temp) {
 					instr_node_succ.push(def_instr.clone());
+				//	println!("in instr def extending {}->{}",node.borrow().id,def_instr.borrow().id);
 				}
-				uses.entry(*instr_read_temp).or_insert(Vec::new()).push(node.clone());
+				uses.entry(*instr_read_temp).or_default().push(node.clone());
 				if !last_uses.contains_key(instr_read_temp) {
 					last_uses.insert(*instr_read_temp, idx);
 				}
@@ -62,10 +67,12 @@ impl InstrDag {
 			// 处理 load call store 指令的依赖关系
 			if instr.is_call() {
 				instr_node_succ.extend(last_loads.iter().cloned());
+			//	println!("in is_call {} extending loads {:?}",node.borrow().id,last_loads.iter().map(|x| x.borrow().id).collect::<Vec<_>>());
 				last_loads.clear();
 				last_call = Some(node.clone());
 			} else if instr.is_load().unwrap_or(false) {
 				if let Some(last_call) = last_call.clone() {
+				//	println!("in is_load {} extending last_call {}",node.borrow().id,last_call.borrow().id);
 					instr_node_succ.push(last_call);
 				}
 				last_loads.push(node.clone());
@@ -79,6 +86,9 @@ impl InstrDag {
 			nodes.push(node);
 		}
 		for node in nodes.iter() {
+			// println!("node id: {}", node.borrow().id);
+			// println!("node successors: {:?}", node.borrow().succ.iter().map(|s| s.borrow().id).collect::<Vec<usize>>());
+			// println!("---------");
 			for succ in node.borrow().succ.iter() {
 				succ.borrow_mut().in_deg += 1;
 			}
