a443bd3b073e53526729efe093a80666aa17b53d
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2024-07-20 13:03:13 +0800

    fix(unreachable): search unreachable blocks reversely from the exit

--- optimizer/src/unreachable/impls.rs
+++ optimizer/src/unreachable/impls.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
+use std::{cell::RefCell, collections::HashSet, rc::Rc};
 
 use super::RemoveUnreachCode;
 use crate::RrvmOptimizer;
-use rrvm::program::LlvmProgram;
+use rrvm::{cfg::BasicBlock, program::LlvmProgram};
 use utils::errors::Result;
 
 impl RrvmOptimizer for RemoveUnreachCode {
@@ -31,6 +31,55 @@ impl RrvmOptimizer for RemoveUnreachCode {
 					false
 				}
 			});
+
+			// 检查是否为单一出口，如不是，则补一个虚拟出口
+			let mut block_has_ret = Vec::new();
+			for bb in cfg.blocks.iter() {
+				if bb.borrow().jump_instr.is_some()
+					&& bb.borrow().jump_instr.as_ref().unwrap().is_ret()
+				{
+					block_has_ret.push(bb.clone());
+				}
+			}
+			let exit;
+			if block_has_ret.len() == 1 {
+				exit = block_has_ret[0].clone();
+			} else {
+				exit = Rc::new(RefCell::new(BasicBlock::new(-1, 0.0)));
+				block_has_ret.iter().for_each(|bb| {
+					bb.borrow_mut().succ.push(exit.clone());
+					exit.borrow_mut().prev.push(bb.clone());
+				});
+			}
+
+			// 复用前面的变量，先清空
+			visited.clear();
+			stack.clear();
+			stack.push(exit);
+
+			// dfs
+			while let Some(u) = stack.pop() {
+				let id = u.borrow().id;
+				visited.insert(id);
+				for v in u.borrow().prev.iter() {
+					if !visited.contains(&v.borrow().id) {
+						stack.push(v.clone())
+					}
+				}
+			}
+			// 清除不可达基本块
+			cfg.blocks.retain(|v| {
+				visited.contains(&v.borrow().id) || {
+					v.borrow_mut().clear();
+					false
+				}
+			});
+			
+			// 清楚虚拟出口的残留影响
+			block_has_ret.iter().for_each(|bb| {
+				bb.borrow_mut().succ.clear();
+			});
+
 			last || size != cfg.blocks.len()
 		}))
 	}
