7f73d2f6bc3b26fd1de97e37b025b5a3541b29c1
ChenghaoLi <lichenghaongyd@qq.com>
2024-08-13 15:31:36 +0000

    feat: runtime function

--- backend/emission/src/lib.rs
+++ backend/emission/src/lib.rs
@@ -20,11 +20,12 @@ pub fn code_emission(program: RiscvProgram, file_name: String) -> String {
 	let data = data.into_iter().map(format_data).collect::<Vec<_>>().join("\n");
 	let bss = bss.into_iter().map(format_bss).collect::<Vec<_>>().join("\n");
 	format!(
-		"{}\n{}{}  .text\n  .global main\n{}\n  .ident {}\n",
+		"{}\n{}{}  .text\n  .global main\n{}\n  .ident {}\n {}",
 		program_head(file_name),
 		set_section("  .section	.sbss, \"aw\", @nobits", bss),
 		set_section("  .section	.sdata, \"aw\"", data),
 		funcs,
-		PROGRAM_IDENT
+		PROGRAM_IDENT,
+		RUNTIME_FUNCTION
 	)
 }
