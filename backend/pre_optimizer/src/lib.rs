73ac2bda2c5ada701b06bc8774384e550321b625
Rosayxy <xy-yu22@mails.tsinghua.edu.cn>
2024-08-19 09:19:18 +0800

    feat: add debug info

--- backend/pre_optimizer/src/lib.rs
+++ backend/pre_optimizer/src/lib.rs
@@ -10,6 +10,7 @@ pub fn prereg_backend_optimize(program: &mut RiscvProgram, level: i32) {
 	branch_combine::branch_combine(program);
 	modify_load_imm::modify_load_imm(program);
 	la_reduce::la_reduce(program);
-	shift_add::shift_add(program);
-	// instruction_scheduling::instr_schedule_program(program);
+	// shift_add::shift_add(program);
+	println!("program before : {}", program);
+	instruction_scheduling::instr_schedule_program(program);
 }
